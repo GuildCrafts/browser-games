bdce917aa954da267ed326977eb3d614b07d1461
Helen Yau <helenyau13@gmail.com>
2017-04-13 13:59:25 -0700

    add jquery highlight effect to buttons and add sound to that corresponding button

--- public/js/simon.js
+++ public/js/simon.js
@@ -1,3 +1,65 @@
-function workpls() {
-  alert('this shit works, wooot!')
+// let start = false
+userCount = 0;
+aiCount = 0;
+
+function startGame() {
+  let empty = []
+  let count = 0;
+
+  let options = ['blue', 'salmon', 'pink', 'green']
+
+  let sounds = {
+    'blue': document.getElementById('sound1'),
+    'salmon': document.getElementById('sound2'),
+    'pink': document.getElementById('sound3'),
+   'green': document.getElementById('sound4')
+  }
+
+  let randomSound = Math.floor(Math.random()*4)
+  console.log('randomSound', randomSound);
+  return options[randomSound]
+  console.log('options[randomSound]', options[randomSound]);
+  let counter = 0
+  for(let i = 0; i < 4; i++) {
+    setTimeout(function() {
+
+      sounds[options[randomSound]].play()
+    }, 1000 * i)
+    counter++
+    }
+  }
+
+  // var i = 0;
+  // for(let key in sounds) {
+  //
+  //   setTimeout(function() {
+  //
+  //     sounds[options[randomSound]].play()
+  //   }, 1000 * i)
+  //   i++
+  // }
+
+
+$(document).ready(function() {
+  $('#topleft').click(function() {
+    $(this).effect("highlight", {color: '#cff5fc'}, 1000);
+    console.log('usercount', userCount);
+    userCount++
+    })
+  $('#topright').click(function() {
+    $(this).effect("highlight", {color: '#ffebe5'}, 1000);
+    userCount++
+    })
+  $('#bottomleft').click(function() {
+    $(this).effect("highlight", {color: '#ffd6fc'}, 1000);
+    userCount++
+    })
+  $('#bottomright').click(function() {
+    $(this).effect("highlight", {color: '#ebffc9'}, 1000);
+    userCount++
+    })
+});
+
+function strictMode() {
+
 }
