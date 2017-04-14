395f675fcc4fa27857ebc13fa760042fe0f8b033
Helen Yau <helenyau13@gmail.com>
2017-04-14 10:51:48 -0700

    create object of colors to match with aisound plays

--- public/js/simon.js
+++ public/js/simon.js
@@ -1,6 +1,5 @@
 let userPlays = []
 let aiPlays = []
-let aiCount = 0
 
 let options = ['blue', 'salmon', 'pink', 'green']
 
@@ -10,55 +9,48 @@ let sounds = {
   'pink': document.getElementById('sound3'),
   'green': document.getElementById('sound4')
 }
+let colorOfSounds = {
+  'blue': '#cff5fc',
+  'salmon': '#ffebe5',
+  'pink': '#ffd6fc',
+  'green': '#ebffc9'
+}
 
 function startGame() {
   let randomNumber = Math.floor(Math.random()*4)
-
   userPlays = []
   aiPlays.push(options[randomNumber])
+  console.log('aiPlays[]', aiPlays);
   for(let i = 0; i < aiPlays.length; i++) {
-    console.log('i', i);
     timeoutSound(i)
+  
   }
   usersTurn()
 }
 
 function timeoutSound(i) {
-  let counter = 0
-  setTimeout(function() {
+  setInterval(function() {
     sounds[aiPlays[i]].play()
-    $('#'+aiPlays[i]).effect('highlight', {}, 2000)
-    // console.log('aiplayyyy', aiPlays);
-  }, 2000)
-  counter++
+    $('#'+aiPlays[i]).effect('highlight', {color: colorOfSounds[aiPlays[i]]}, 1000)
+  }, 1000)
 }
 
 function aiTurn() {
-  console.log('aiplays', aiPlays)
   console.log('userplays', userPlays);
   if(JSON.stringify(aiPlays) === JSON.stringify(userPlays)) {
     startGame()
     userPlays = []
   }
   else {
-    console.log('failed')
+    alert('booooooo, you lose!')
     userPlays = []
   }
 }
-  // var i = 0;
-  // for(let key in sounds) {
-  //
-  //   setTimeout(function() {
-  //
-  //     sounds[options[randomSound]].play()
-  //   }, 1000 * i)
-  //   i++
-  // }
 
 
 function usersTurn() {
   $(document).ready(function() {
-    $('#blue').unbind('click').bind('click', function() {
+    $('#blue').unbind().click(function() {
       $(this).effect("highlight", {color: '#cff5fc'}, 1000);
       sounds[options[0]].play()
       userPlays.push(options[0])
@@ -67,7 +59,7 @@ function usersTurn() {
         userPlays = []
       }
       })
-    $('#salmon').unbind('click').bind('click', function() {
+    $('#salmon').unbind().click(function() {
       $(this).effect("highlight", {color: '#ffebe5'}, 1000);
       sounds[options[1]].play()
       userPlays.push(options[1])
@@ -77,7 +69,7 @@ function usersTurn() {
       }
       })
 
-    $('#pink').unbind('click').bind('click',function() {
+    $('#pink').unbind().click(function() {
       $(this).effect("highlight", {color: '#ffd6fc'}, 1000);
       sounds[options[2]].play()
       userPlays.push(options[2])
@@ -86,7 +78,7 @@ function usersTurn() {
         userPlays = []
       }
       })
-    $('#green').unbind('click').bind('click', function () {
+    $('#green').unbind().click(function () {
       $(this).effect("highlight", {color: '#ebffc9'}, 1000);
       sounds[options[3]].play()
       userPlays.push(options[3])
@@ -100,8 +92,4 @@ function usersTurn() {
 
 function endTurn() {
   return userPlays.length === aiPlays.length
-}
-
-function strictMode() {
-
 }
