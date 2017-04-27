783d7d8b8164cfaf440b9ace0ce075284032b71e
Roel <roel.cabrera@gmail.com>
2017-04-26 22:19:25 -0700

    update not much done jQuery doesnt like me very much

--- public/Simon.js
+++ public/Simon.js
@@ -3,31 +3,33 @@ $(document).ready(function () {
   var memoryArray = [];
   var levelCount = 0;
 
-  //win check
-//  function wincheck()
-//  if (levelCount >= 20) {
-//    console.log("You win");
-//  };
-
   //Counter display
   function update_levelCount() {
-    $('#count span').text(levelCount)
+    $('#count span').text(levelCount);
   };
 
 
   //computer turn
   function memoryturn() {
     var num = Math.floor(Math.random() * 4);
-    var numRan = "#" + num;
-    var c = jQuery.Event('click');
+    for (var i = 0; i <= memoryArray; i++){
+      $(memoryArray[i]).trigger('click');
+      console.log (memoryArray);
+    };
+    $("#" + num).trigger('click', true);
+    memoryArray.push(num);
     levelCount++;
-      $(numRan).triggerHandler('c');
-
-
-
 
   };
-
+  function wincheck(){
+    if (turnCount=20) {
+      console.log("You Win!");
+      userArray=[];
+      return;
+    };
+    userArray=[];
+    memoryturn();
+  };
 
   //Restart button
   $(".newgame").on('click', function () {
@@ -36,18 +38,26 @@ $(document).ready(function () {
     levelCount = 0;
     update_levelCount();
     memoryturn();
-
   });
 
   // clicking on the buttons
-  $(".but").on('click', function () {
+  $(".but").on('click', function(e,skip){
+    if(skip) return ;
     userArray.push(this.id);
-    if (userArray !== memoryArray) {
+    var a = userArray.toString();
+    var b = memoryArray.toString();
+
+    if ( a === b ) {
+      console.log("yay");
+      wincheck();
+
+    } else{
+      console.log("no");
 
     };
+      console.log("user" + a);
+      console.log("comp" + b);
 
-  });
-
-
+    });
 
 });
