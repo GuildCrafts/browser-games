d8fbee19900950fa99a032e04979f412c31a3b57
Roel <roel.cabrera@gmail.com>
2017-04-28 01:52:17 -0700

    Simon almost done just need to work on sequence timing

--- public/Simon.js
+++ public/Simon.js
@@ -1,119 +1,153 @@
 $(document).ready(function () {
-      var userSequence = [];
-      var comSequence = [];
-      var countSeq = 0;
-      var strictMode = false;
+  var userSequence = [];
+  var comSequence = [];
+  var countSeq = 0;
+  var strictMode = false;
 
 
-// original game mode
-      $('#btn-original').on('click', function(){
-        $(this).addClass('.buttonmode');
-        strictMode = false;
-        $('#display').text("ORIGINAL MODE");
-        if ($('#btn-strict').hasClass('.buttonmode')){
-          $('#btn-strict').removeClass('.buttonmode');
-        } else{
-          return;
-        }
-      })
-
-
-//strict mode button
-      $('#btn-strict').on('click',function(){
-        $(this).addClass('.buttonmode');
-        strictMode = true;
-        $('#display').text("STRICT MODE");
-        if ($('#btn-original').hasClass('.buttonmode')){
-          $('#btn-original').removeClass('.buttonmode');
-        } else{
-          return;
-        }
-
-
+  //Red sounds
+  $('.red').on('click', function () {
+    $('#redsound')[0].play()
   })
 
 
-// Check if strict or normal mode
-      function strict_orig() {
-        if (strict = true) {
-          //something about restarting game if wincheck is false
-        } // i dont think i will need an else statment but i could be wrong
+  // blue sounds
+  $('.blue').on('click', function () {
+    $('#bluesound')[0].play()
+  })
 
-      }
+
+  // yellow sounds
+  $('.yellow').on('click', function () {
+    $('#yellowsound')[0].play()
+  })
+
+
+  // green sounds
+  $('.green').on('click', function () {
+    $('#greensound')[0].play()
+  })
+
+
+  $('.mode').on('click', function ($('.mode').toggleClass('.selected')));
+    if (strictMode === true){
+      strictMode = false;
+    } else
+     strictMode = true;
+})
+
 
 
 //counter display
-      function update_count() {
-        countSeq++;
-        $('#count').text(countSeq);
-      }
+function update_count() {
+  countSeq++;
+  $('#count').text(countSeq);
+}
 
 
 //win check
-      function win_check() {
-        if (countSeq >= 20) {
-          $('#display').text("YOU WIN!")
-        } else {
-          userSequence = [];
-        }
-      }
+function win_check() {
+  if (countSeq >= 20) {
+    $('#display').text("YOU WIN!")
+  } else {
+    userSequence = [];
+  }
+}
+
+
+// play comArray sequence...hopefully
+function playSequence() {
+  for (var i = 0; i <= comSequence.length; i++) {
+    if (comSequence[i] === 0) {
+      $('#0').addClass('redclick').delay(2000).queue(function (next) {
+        $('#0').removeClass('redclick');
+        next();
+        $('#redsound')[0].play();
+      })
+    } else if (comSequence[i] === 1) {
+      $('#1').addClass('blueclick').delay(2000).queue(function (next) {
+        $('#1').removeClass('blueclick');
+        next();
+        $('#yellowsound')[0].play();
+      })
+    } else if (comSequence[i] === 2) {
+      $('#2').addClass('yellowclick').delay(2000).queue(function (next) {
+        $('#2').removeClass('yellowclick');
+        next();
+        $('#greensound')[0].play();
+      })
+    } else if (comSequence[i] === 3) {
+      $('#3').addClass('greenclick').delay(2000).queue(function (next) {
+        $('#3').removeClass('greenclick');
+        next();
+        $('#greensound')[0].play()
+      })
+    } else {
+      console.log("you fucked up somewhere coding");
+    }
+  }
+}
+
 
 
 //Computer turn
-      function comTurn(){
-        var ranNum = Math.floor(Math.random()*4);
-        var randomID = "#" + ranNum;
-        comSequence.push(ranNum);
-        update_count();
-        console.log(comSequence);
-        $('#display').text("WATCH CARFULLY...")
-      }
+function comTurn() {
+  var ranNum = Math.floor(Math.random() * 4);
+  var randomID = "#" + ranNum;
+  comSequence.push(ranNum);
+  update_count();
+  console.log(comSequence);
+  $('#display').text("WATCH CARFULLY...")
+  playSequence();
+}
+
 
 
 // Computer to repeat sequence if user entered incorrect sequence
-  //Original mode only
-      function sequence_repeat(){
+//Original mode only
+function sequence_repeat() {
 
-      }
+}
 
 
 //Start Button
-      $('#start').on('click', function () {
-        userSequence = [];
-        comSequence = [];
-        countSeq = 0;
-        comTurn();
-        $('#display').text("GAME START")
-      })
+$('#start').on('click', function () {
+  userSequence = [];
+  comSequence = [];
+  countSeq = 0;
+  comTurn();
+  $('#display').text("GAME START")
+})
 
 
 //Sequence Check
-      function sequence_check() {
-        if (userSequence.toString() === comSequence.toString()) {
-          win_check();
-          console.log("yay")
-          comTurn();
-        } else if (strictMode === false){
-            userSequence=[];
-            sequence_repeat();
-            $('#display').text("TRY AGAIN")
-          } else {
-            userSequence=[];
-            comSequence=[];
-            countSeq= 0;
-          }
-        }
-
+function sequence_check() {
+  if (userSequence.toString() === comSequence.toString()) {
+    win_check();
+    console.log("yay")
+    comTurn();
+  } else if (strictMode === false) {
+    userSequence = [];
+    sequence_repeat();
+    $('#losesound')[0].play();
+    $('#display').text("TRY AGAIN")
+    playSequence();
+  } else {
+    $('#losesound')[0].play();
+    userSequence = [];
+    comSequence = [];
+    countSeq = 0;
+    comTurn();
+  }
+}
 
 
 // coloured button click and added to sequence
-      $('.but').on('click', function() {
-        $(this).addClass('redclick').delay(500).removeClass('redclick');
-        userSequence.push(this.id);
-        if (userSequence.length === comSequence.length){
-          sequence_check();
-        }
-        })
-
-
-})
+$('.but').on('click', function () {
+$(this).addClass('redclick').delay(500).removeClass('redclick');
+userSequence.push(this.id);
+if (userSequence.length === comSequence.length) {
+  sequence_check();
+}
+});
+});
--- public/ticTacToe.js
+++ public/ticTacToe.js
@@ -1,54 +1,17 @@
-
-
-
 $(document).ready(function(){
-//"0" "1" "2"
-//"3" "4" "5"
-//"6" "7" "8"
+var userArr = [];
+var comArr = [];
+var complete = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var usedArr = userArr + comArr;
+  console.log(usedArr);
+});
 
-//var hash = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-var tic = []; // X player
-var tac = []; // O computer
-var winSet = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-var turn = 0;
-
-//computer play
-function turnAI(){
-    if (turn % 2 < 1)
-    if ($(this).hasClass('box-disabled')) return;
-    var num = Math.floor(Math.random()*9) - 1;
-    if ()
-    $(this).addClass('box-disabled', 'marked');
-  });
-};
-
-function turnCounter();
-  turn += 1;
-  console.log turn;
-  if (turn >=9){
-  console.log("Game Over!");
+$('.flex-box').on('click', function(){
+  userArr.push(this.id);
+  if (this).hasClass('.disabled') return;
+  if (this).hasclass('.clean'){
+    (this).removeClass('.clean');
   }
-//win check
-function winCheck() {
- if (tic.indexOf([0, 1, 2], 0) <= -1){
-   console.log ("Game Over!");
- } else {
-
- }
-}
-
-//player choice
-$(".flex-box").on('click',function() {
-  if($(this).hasClass("disabled")) return; // no no no not today
-  $(this).removeClass('clean');
-  document.getElementById("this").innerHTML = X;
   $(this).addClass('box-disabled', 'marked');
-});
 
-
-$('.btn-danger').click(function reset(){
-    location.reload();
-
-});
-
-});
+})
