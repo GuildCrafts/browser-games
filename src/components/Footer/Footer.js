9e3c92f9a08eab024c048be20c32ded635ab9700
mikeadossi <mikeadossi@gmail.com>
2017-05-01 21:35:40 -0700

    footer content added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css'
+
+export default class Footer extends React.Component{
+  render(){
+    return(
+      <div className="footer">
+        <div className="footer_container"><p className="footer_title">Footer</p></div>
+      <div className="footer_top_section"></div>
+      </div>
+    )
+  }
+}
