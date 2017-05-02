57929166a405e484788cbe61080cf4369ab99e6a
mikeadossi <mikeadossi@gmail.com>
2017-05-02 02:01:16 -0700

    create ConnectFour component

---
+++ src/components/ConnectFour/ConnectFour.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './connectFour.css'
+
+export default class ConnectFour extends React.Component{
+  render(){
+    return(
+      <div>Connect Four game</div>
+    )
+  }
+}
