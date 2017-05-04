506e08918ccfb43c6d13df1a655293e24579b50b
mikeadossi <mikeadossi@gmail.com>
2017-05-03 18:44:03 -0700

    write simon styling

--- src/components/Simon/Simon.js
+++ src/components/Simon/Simon.js
@@ -4,7 +4,21 @@ import './simon.css';
 export default class Simon extends React.Component{
   render(){
     return(
-      <div>Simon Game</div>
+      <div className="simon">
+        <div className="simon_container">
+          <div className="simon_interface">
+            <div className="simon_horizontal_line"></div>
+            <div className="simon_vertical_line"></div>
+          </div>
+          <div className="redBox"></div>
+          <div className="blueBox"></div>
+          <div className="greenBox"></div>
+          <div className="yellowBox"></div>
+          <div className="simon_controls_circle">
+            <div className="simon_title">Simon</div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
