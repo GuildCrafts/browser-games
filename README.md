239835079865742025cb8a51d274ac0d50f49d29
Helen Yau <helenyau13@gmail.com>
2017-05-04 09:58:27 -0700

    fix the issue where the player jumps linearly rather than at a curve and add platforms to make the game more playable

--- README.md
+++ README.md
@@ -22,3 +22,29 @@ Available on:
 ```
 
 Then open `http://localhost:4321/` in your browser of choice and play away!
+
+### Specifications
+
+## General
+
+ - [ ] Artifact produced is a fork of the browser-games repo.
+ - [ ] Variables, functions, files, etc. have appropriate and meaningful names.
+ - [ ] HTML, CSS, and JS files are well formatted with proper spacing and indentation.
+ - [ ] There is a clear separation of game logic code from view/rendering code.
+ - [ ] All major features are added via pull requests with a clear description and concise commit messages.
+ - [ ] The artifact produced is properly licensed, preferably with the MIT license.
+
+## Generic Platform Game
+
+- [ ] Game can be found at public/platform.html
+- [ ] Game is playable by one player
+- [ ] Game follows rules established in tutorial
+- [ ] Game page is linked from public/index.html
+
+## Stretch
+
+Design and build your own platform-like game. What else can you build with the techniques you came up with in building the Generic Platform Game?
+
+ - [ ] Game has its own HTML, CSS, and JS
+ - [ ] Game is playable
+ - [ ] Game page is linked from public/index.html
--- public/js/platform.js
+++ public/js/platform.js
@@ -1,6 +1,8 @@
+let canvas = document.createElement('canvas');
 let ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 480;
+canvas.width = 600;
+canvas.height = 380;
+document.body.appendChild(canvas);
 
 let bgReady = false;
 let bgImage = new Image()
@@ -15,25 +17,81 @@ let heroImage = new Image();
 heroImage.onload = function () {
   heroReady = true
 };
-heroImage.src = "./img/girl2_1.png";
+heroImage.src = "./img/girl2_2.png";
 
 let monsterReady = false;
 let monsterImage = new Image();
 monsterImage.onload = function () {
   monsterReady = true
 };
-monsterImage.src = "./img/idle_1.png";
+monsterImage.src = "./img/idle_2.png";
+
+let platformReady = false;
+let platformImage = new Image();
+platformImage.onload = function () {
+  platformReady = true
+}
+platformImage.src = "./img/cake.png"
+
+let platformReady1 = false;
+let platformImage1 = new Image()
+ platformImage1.onload = function () {
+   platformReady1 = true
+ }
+ platformImage1.src = "./img/cake1.png"
+
+ let platformReady2 = false;
+ let platformImage2 = new Image()
+ platformImage2.onload = function () {
+   platformReady2 = true
+ }
+ platformImage2.src = "./img/cake2.png"
+
+ let platformReady3 = false;
+ let platformImage3 = new Image()
+ platformImage3.onload = function () {
+   platformReady3 = true
+ }
+ platformImage3.src = "./img/cake3.png"
+
+let platformReady4 = false
+let platformImage4 = new Image()
+platformImage4.onload = function () {
+  platformReady4 = true
+}
+platformImage4.src = "./img/cakeCenter.png"
+
+let platformReady5 = false
+let platformImage5 = new Image()
+platformImage5.onload = function () {
+  platformReady5 = true
+}
+platformImage5.src = "./img/cakeHillLeft1.png"
+
+
 
 let hero = {
-  speed: 256,
+  speed: 150,
   x: 0,
-  y: 0
-};
+  y: 0,
+  jumping: false,
+  upwardVelocity: 0,
+  width: 120,
+  height: 120
+},
+gravity = 0.2,
+friction = 1,
+gravityDelta = 0.1;
 
 let monster = {
   x: 0,
   y: 0
 };
+
+let platform = {
+  x: 70,
+  y: 310
+}
 
 let monstersCaught = 0;
 
@@ -48,16 +106,36 @@ addEventListener("keyup", function (e) {
 }, false)
 
 let reset = function () {
-  hero.x = canvas.width - 750
-  hero.y = canvas.height - 100
+  hero.x = canvas.width - 600
+  hero.y = canvas.height - 105
 
-  monster.x = 32 + (Math.random() * (canvas.width - 64))
-  monster.y = 32 + (Math.random() * (canvas.height - 64))
+  monster.x = (Math.random() * (canvas.width - 250))
+  monster.y = (Math.random() * (canvas.height - 100))
 };
 
 let update = function (modifier) {
-  if(38 in keysDown) {
-    hero.y -= hero.speed * modifier
+  if(32 in keysDown) {
+    if(hero.upwardVelocity === 0) {
+      hero.upwardVelocity = 5
+    }
+  }
+  if (hero.upwardVelocity > 0) hero.upwardVelocity -= gravityDelta
+  if (hero.upwardVelocity < 0) hero.upwardVelocity = 0
+  hero.y += gravity + gravityDelta - hero.upwardVelocity
+  gravityDelta *= 1.10
+
+  console.log('FRAME', hero)
+
+  if(hero.x >= canvas.width-hero.width) {
+    hero.x = canvas.width-hero.width
+  } else if(hero.x <= 0) {
+    hero.y = 380
+  }
+
+  if(hero.y >= canvas.height- hero.height ) {
+    hero.y = canvas.height -hero.height
+    hero.jumping = false;
+    gravityDelta = 0.1
   }
   if(40 in keysDown) {
     hero.y += hero.speed * modifier
@@ -78,6 +156,8 @@ let update = function (modifier) {
     ++monstersCaught;
     reset()
   }
+
+
 };
 
 let render = function () {
@@ -91,6 +171,32 @@ let render = function () {
   if(monsterReady) {
     ctx.drawImage(monsterImage, monster.x, monster.y);
   }
+
+  if(platformReady) {
+  ctx.drawImage(platformImage, 0, 310)
+  }
+
+  if(platformReady1) {
+    ctx.drawImage(platformImage1, platform.x, platform.y)
+  }
+
+  if(platformReady2) {
+    ctx.drawImage(platformImage2, 140, 310)
+  }
+
+  if(platformReady3) {
+    ctx.drawImage(platformImage3, 210, 310)
+  }
+
+  if(platformReady4) {
+    ctx.drawImage(platformImage4, 280, 310)
+  }
+
+  if(platformReady5) {
+    ctx.drawImage(platformImage5, 261, 240)
+  }
+
+
 
   ctx.fillStyle = "#C99AFE"
   ctx.font = "24px Helvetica"
