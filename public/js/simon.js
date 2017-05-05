cba2fd4cb74d56f6a756db2f321a907b6777a10f
SomayaB <somaya.bounouar@gmail.com>
2017-05-05 14:50:50 -0700

    Add Simon game functionality for 1 round

--- public/js/simon.js
+++ public/js/simon.js
@@ -1,38 +1,41 @@
 
-
+var sequence = []
+var copy = []
+var round = 0
+var active = true
 
 var startGame = function() {
-  this.sequence = [];
-  this.copy = [];
-  this.round = 0;
-  this.active = true;
   document.querySelector('[data-action="lose"]').style.visibility = "hidden"
-  this.newRound();
+  sequence = []
+  copy = []
+  round = 0
+  active = true
+  newRound()
 }
 
 var newRound = function() {
-	document.querySelector('[data-round]').innerHTML = ++this.round;
-	this.sequence.push(this.randomNumber());
-	this.copy = this.sequence.slice(0);
-	this.animate(this.sequence);
+	document.querySelector('[data-round]').innerHTML = ++round
+	sequence.push(randomNumber())
+	copy = sequence.slice(0)
+	animate(sequence)
 }
 
 var randomNumber = function() {
-		return Math.floor((Math.random()*4)+1);
-	}
+		return Math.floor((Math.random()*4)+1)
+}
 
 var animate = function(sequence) {
-	var i = 0;
+	var i = 0
 	var interval = setInterval(function() {
-		playSound(sequence[i]);
-		lightUp(sequence[i]);
+		playSound(sequence[i])
+		lightUp(sequence[i])
 
-		i++;
+		i++
 		if (i >= sequence.length) {
-			clearInterval(interval);
-			activateSimonBoard();
+			clearInterval(interval)
+			activateSimonBoard()
 		}
-	}, 600);
+	}, 600)
 }
 
 var playSound = function(tile) {
@@ -51,10 +54,10 @@ var playSound = function(tile) {
 
 var lightUp = function(tile) {
 	var tileDom = document.querySelector('[data-tile="' + tile + '"]')
-  tileDom.className += ' lit';
+  tileDom.className += ' lit'
 	window.setTimeout(function() {
-		tileDom.classList.remove('lit');
-	}, 300);
+		tileDom.classList.remove('lit')
+	}, 300)
 }
 
 var activateSimonBoard = function() {
@@ -62,192 +65,40 @@ var activateSimonBoard = function() {
     let num = event.target.getAttribute('data-tile')
     registerClick(num)
   })
+  document.querySelector('[data-tile]').className += ' hoverable'
 }
-
-//
-//
-// 				.on('mousedown', '[data-tile]', function(){
-// 					$(this).addClass('active');
-// 					that.playSound($(this).data('tile'));
-// 				})
-//
-// 				.on('mouseup', '[data-tile]', function(){
-// 					$(this).removeClass('active');
-// 				});
-//
-// 			$('[data-tile]').addClass('hoverable');
-// 		},
 
 var registerClick = function(num) {
-	var desiredResponse = this.copy.shift();
+	var desiredResponse = copy.shift()
 	var actualResponse = num
-	this.active = (desiredResponse === actualResponse);
-	this.checkLose();
+	active = (desiredResponse == actualResponse)
+	checkLose()
 }
+
+var checkLose = function() {
+  if (copy.length === 0 && active) {
+			deactivateSimonBoard()
+			newRound()
+	} else if (!active) {
+  		deactivateSimonBoard()
+  		endGame()
+	}
+}
+
+var deactivateSimonBoard = function() {
+	document.querySelector('.tile').removeEventListener('click', function() {
+  })
+  document.querySelector('[data-tile]').classList.remove('hoverable')
+}
+
+var endGame = function() {
+  document.querySelector('[data-action="lose"]').style.visibility = "visible"
+}
+
+
 
 window.onload = function() {
   document.querySelector('[data-action=start]').addEventListener('click', function() {
-    startGame();
+    startGame()
   })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// 		// add a new color to the sequence and animate it to the user
-// 		newRound: function() {
-// 			document.querySelector('.round').innerText(++this.round);
-// 			this.sequence.push(this.randomNumber());
-// 			this.copy = this.sequence.slice(0);
-// 			this.animate(this.sequence);
-// 		},
-//
-// 		// the game is controlled primarily through this function, along with checkLose().
-// 		// Since the player can never actually "win", we just listen for clicks as the user
-// 		// plays the sequence and each time, check if they lost
-// 		registerClick: function(event) {
-// 			var desiredResponse = this.copy.shift();
-// 			// var actualResponse = $(event.target).data('tile');
-//       // //document..data
-// 			// this.active = (desiredResponse === actualResponse);
-// 			// this.checkLose();
-// 		},
-//
-// 		// three possible situations:
-// 		// 1. The user clicked the wrong color (end the game)
-// 		// 2. The user entered the right color, but is not finished with the sequence (do nothing)
-// 		// 3. The user entered the right color and just completed the sequence (start a new round)
-// 		checkLose: function() {
-// 			// copy array will be empty when user has successfully completed sequence
-// 			if (this.copy.length === 0 && this.active) {
-// 				this.deactivateSimonBoard();
-// 				this.newRound();
-//
-// 			} else if (!this.active) { // user lost
-// 				this.deactivateSimonBoard();
-// 				this.endGame();
-// 			}
-// 		},
-//
-// 		endGame: function() {
-// 			// notify the user that they lost and change the "round" text to zero
-// 			document.querySelector('.lose').show();
-// 			document.querySelector('.round').get(0).text('0');
-// 		},
-//
-// 		/*----------------- Helper functions -------------------*/
-//
-// 		// allow user to interact with the game
-// 		activateSimonBoard: function() {
-// 			var that = this;
-// 			document.querySelector('.tile').onclick.console.log('hi')
-//       // $('.simon')
-//       //     .on('click', '[data-tile]', function(e) {
-// 			// 		that.registerClick(e);
-// 			// 	})
-//
-// 				.on('mousedown', '[data-tile]', function(){
-// 					$(this).addClass('active');
-// 					that.playSound($(this).data('tile'));
-// 				})
-//
-// 				.on('mouseup', '[data-tile]', function(){
-// 					$(this).removeClass('active');
-// 				});
-//
-// 			$('[data-tile]').addClass('hoverable');
-// 		},
-//
-// 		// prevent user from interacting until sequence is done animating
-// 		deactivateSimonBoard: function() {
-// 				document.querySelector('.simon')
-// 					.off('click', '[data-tile]')
-// 					.off('mousedown', '[data-tile]')
-// 					.off('mouseup', '[data-tile]');
-//
-// 				$('[data-tile]').removeClass('hoverable');
-// 		},
-//
-// 		animate: function(sequence) {
-// 			var i = 0;
-// 			var that = this;
-// 			var interval = setInterval(function() {
-// 				that.playSound(sequence[i]);
-// 				that.lightUp(sequence[i]);
-//
-// 				i++;
-// 				if (i >= sequence.length) {
-// 					clearInterval(interval);
-// 					that.activateSimonBoard();
-// 				}
-// 			}, 600);
-// 		},
-//
-// 		lightUp: function(tile) {
-// 				var $tile = $('[data-tile=' + tile + ']').addClass('lit');
-// 				window.setTimeout(function() {
-// 					$tile.removeClass('lit');
-// 				}, 300);
-// 		},
-//
-// 		// we are embedding the sound file on the fly for the following benefits:
-// 		// 1. ability to play multiple sounds in a row without waiting for the first to complete,
-// 		// 2. <audio> tag provides our fallbacks (ogg, mp3).
-// 		playSound: function(tile) {
-// 				var audio = $('<audio autoplay></audio>');
-// 				audio.append('<source src="sounds/' + tile + '.ogg" type="audio/ogg" />');
-// 				audio.append('<source src="sounds/' + tile + '.mp3" type="audio/mp3" />');
-// 				$('[data-action=sound]').html(audio);
-// 		},
-//
-// 		randomNumber: function() {
-// 			// between 1 and 4
-// 			return Math.floor((Math.random()*4)+1);
-// 		}
-// 	};
-//
-//
-// // const SimonGame = new Simon
-//
-// $('document').ready(function(){
-//
-//   // $('.tile').click(function() {
-//   //   console.log('hiiiii')
-//   // })
-//
-//   var sequence = [],
-//       copy = [],
-//       round = 0,
-//       active = true;
-//
-//   document.querySelector('.start').addEventListener('click', function() {
-//     startGame();
-//   })
-//
-//   console.log(Simon);
-//
-//   // document.getElementsByClassName('tile').addEventListener('click', function() {
-//   //   console.log('clicked!!')
-//   // })
-//
-// })
-//
-// var startGame = function() {
-//   this.sequence = [];
-//   this.copy = [];
-//   this.round = 0;
-//   this.active = true;
-//   // $('p[data-action="lose"]').hide();
-//   document.querySelector('.lose').style.visibility = "hidden"
-//   this.newRound();
-// }
