1e4689a38707f7d8d74a425286b3f894c8920de2
Jose Moreno <josemoreno@Joses-MacBook-Air.local>
2017-05-09 17:49:08 -0700

    Add structure to tictactoe html, and 3x3 grid

---
+++ public/js/ticTacToeScript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+//Game begins, X turn
+  var turn = "X";
+  //Arrays stores values that will check later for a winner
+  var turns = {"#","#","#","#","#","#","#","#","#"}
+
+
+});
