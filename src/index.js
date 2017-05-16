831a201251204b136cd3217783a264092f3e3c6b
TrevorJamesH <jetsamrover@gmail.com>
2017-05-16 11:06:25 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Game from './App';
+import GameSelect from './App';
 // import './index.css';
 
 ReactDOM.render(
-  <Game />,
+  <GameSelect />,
   document.getElementById('root')
 );
