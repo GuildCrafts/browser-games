64d90869f7b575f65904945207d35546ab027ed4
Max Antinone <mantinone@gmail.com>
2017-05-16 15:29:20 -0700

    Board exists and renders

--- public/js/chess.js
+++ public/js/chess.js
@@ -1,5 +1,22 @@
-// console.log();
+const board = new Board( 8, 8 )
+
 $(document).ready( () => {
-  $('#Stuff').text('Hi') 
-  console.log("We did things");
+  $('#Stuff').text('Hi')
+  board.map2d( appendSquare, appendCol )
 })
+
+const appendCol = ( col, x ) => {
+  $('#board').append(`<div class="board-col" id=col-${x}></div>`)
+}
+
+const appendSquare = ( space, x, y ) => {
+  x = parseInt(x)
+  y = parseInt(y)
+  let color = (x+y)%2 > 0 ? 'white-space' : 'black-space'
+  $(`#col-${x}`).append(
+    `<div class="board-square ${color} id=${x},${y}"></div>`)
+}
+
+const boardTest = ( space, x, y ) => {
+  console.log(space);
+}
---
+++ public/js/chessClasses/board.js
@@ -0,0 +1,41 @@
+class Board {
+  constructor( x, y ) {
+    this.grid = []
+    for( let i = 0; i < x; i++ ){
+      this.grid[i] = []
+      for( let j = 0; j < y; j++){
+        this.grid[i][j] = 'x'
+      }
+    }
+  }
+
+  getSpace( coord ){
+    let [x,y] = this.translateCoordinate( coord )
+
+    return this.grid[x][y]
+  }
+
+  setSpace( coord, val ){
+    let [x,y] = this.translateCoordinate( coord )
+
+    this.grid[x][y] = val
+  }
+
+  translateCoordinate( coord ){
+    const x = coord[0].charCodeAt() - 97
+    const y = coord[1] - 1
+
+    return [x,y]
+  }
+
+   map2d( fxnSquare, fxnRow ) {
+    for( let i in this.grid ){
+      if( fxnRow ){
+        fxnRow( this.grid[i], i, this.grid )
+      }
+      for( let j in this.grid[i] ){
+        fxnSquare( this.grid[i][j], i, j, this.grid )
+      }
+    }
+  }
+}
