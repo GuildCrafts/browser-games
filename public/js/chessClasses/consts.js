d47304f9161eebca922b441cd911503559b115c7
Max Antinone <mantinone@gmail.com>
2017-05-17 12:04:34 -0700

    Creates Piece class and render pieces

---
+++ public/js/chessClasses/consts.js
@@ -0,0 +1,9 @@
+const PAWN = 'pawn'
+const ROOK = 'rook'
+const KNIGHT = 'knight'
+const BISHOP = 'bishop'
+const QUEEN = 'queen'
+const KING = 'king'
+
+const BLACK = 'black'
+const WHITE = 'white'
