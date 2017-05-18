8a8019bef8435e38d6fedb28cf542bf2024b335a
Max Antinone <mantinone@gmail.com>
2017-05-18 10:54:10 -0700

    Implements game manager and piece movement

--- public/js/chess.js
+++ public/js/chess.js
@@ -1,30 +1,42 @@
-const board = new Board( 8, 8 )
-
+const manager = new GameManager()
 
 $(document).ready( () => {
-  $('#Stuff').text('Hi')
-  board.setSquare( 'c2', new Piece())
-  board.setSquare( 'e7', new Piece( KNIGHT, BLACK ) )
-  board.map2d( appendSquare, appendCol )
+  renderBoard( manager.board )
+  $('#board').click( checkMove )
 })
+
+const checkMove = ( event ) => {
+  const clickCoord = event.target.id ?
+    event.target.id :
+    event.target.parentNode.id
+
+  manager.onClick( clickCoord )
+  renderBoard( manager.board )
+}
 
 const appendCol = ( col, x ) => {
   $('#board').append(`<div class="board-col" id=col-${x}></div>`)
 }
 
-const appendSquare = ( space, x, y ) => {
+const appendSquare = ( space, x, y, grid ) => {
   x = parseInt(x)
   y = parseInt(y)
   let color = (x+y)%2 > 0 ? 'white-space' : 'black-space'
+  let coordString = manager.board.xyToLetter( x, y )
 
-  let imgTag = board.grid[x][y] ?
-    `<img class="piece" src=${board.grid[x][y].renderString()}`:
+  let imgTag = grid[x][y] ?
+    `<img class="piece" src=${grid[x][y].renderString()}`:
     ''
 
   $(`#col-${x}`).append(
-    `<div class="board-square ${color} id=${x},${y}">
+    `<div class="board-square ${color}" id=${coordString}>
       ${imgTag}
     </div>`)
+}
+
+const renderBoard = ( aBoard ) => {
+  $('#board').empty()
+  aBoard.map2d( appendSquare, appendCol )
 }
 
 const boardTest = ( space, x, y ) => {
