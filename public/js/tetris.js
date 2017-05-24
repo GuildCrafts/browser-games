20f434468f4250f209443081f1be4b8c2e6ff0c2
Jacklyne Bouier <jacklynebouier@Jacklynes-MacBook-Pro.local>
2017-05-23 17:17:55 -0700

    collide and stack functionality working

--- public/js/tetris.js
+++ public/js/tetris.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
-context.scale(10, 10);
+context.scale(20, 20);
 
 context.fillStyle = "#000";
 context.fillRect(0,0, canvas.width, canvas.height);
@@ -157,11 +157,31 @@ const Z = [
 	]
 ];
 
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.position];
+  for( let y = 0; y < m.length; ++y) {
+    for(let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x])!== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function createMatrix(width, height) {
+  const matrix = [];
+  while(height--) {
+    matrix.push(new Array(width).fill(0));
+  }
+  return matrix;
+}
+
 function draw() {
 
   context.fillStyle = "#000";
   context.fillRect(0,0, canvas.width, canvas.height);
-
+  drawMatrix(arena, {x: 0, y: 0})
   drawMatrix(player.matrix, player.position);
 }
 
@@ -178,6 +198,25 @@ matrix.forEach((row, y) => {
 });
 }
 
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.position.y][x + player.position.x] = value;
+      }
+    });
+  });
+}
+function playerDrop() {
+  player.position.y++;
+  if(collide(arena, player)) {
+    player.position.y--;
+    merge(arena, player);
+    player.position.y = 0;
+  }
+  dropCounter = 0;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 let lastTime = 0;
@@ -188,16 +227,30 @@ function update(time = 0) {
 
   dropCounter += deltaTime;
   if (dropCounter > dropInterval) {
-    player.position.y++;
-    dropCounter = 0;
+    playerDrop();
+
   }
   draw();
   requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12,20);
+
+let matrix = I[0];
+
 const player = {
   position:{x:5, y:5},
-  matrix:I[0],
+  matrix:matrix,
 }
+
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 37) {
+      player.position.x--;
+  } else if ( event.keyCode === 39) {
+      player.position.x++;
+  } else if ( event.keyCode === 40)
+    playerDrop();
+
+});
 
 update();
