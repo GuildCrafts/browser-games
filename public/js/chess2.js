1a7e7b875b1dcacc5141c2a0ccc6cd653c6f650e
John <jhallman5@gmail.com>
2017-05-25 12:18:56 -0700

    creates the chess board

--- public/js/chess2.js
+++ public/js/chess2.js
@@ -1,17 +1,24 @@
 $(document).ready( function(){
 
-  const generateRow = () => {
-    for(let i = 0; i < 8; i++) {
-      $('.board').append('<div class="square '+ `column${i}` + ' "></div>')
-    }
-  }
   const generateBoard = () => {
-    for( let i = 0; i < 8; i ++) {
-      generateRow()
+    for(let i = 0; i < 8; i++) {
+      for(let j = 0; j < 8; j++) {
+        let columnPattern;
+        if(i % 2 === 0 ){
+          columnPattern = 'white'
+        } else {
+          columnPattern = 'black'
+        }
+        const square = `<div class=square
+          data-pattern=${columnPattern}
+          data-column=${i}
+          data-row=${j}>
+        </div>`
+        $('.board').append(square)
+      }
     }
   }
+
   generateBoard()
-
-
 
 })
